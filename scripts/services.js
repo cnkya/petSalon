89f9d42da04e4d788ddbca26705aefb9cf28918a
Christina Nkya <mrs.nkya@gmail.com>
2024-06-06 20:06:59 -0400

    105 assignment3

--- scripts/services.js
+++ scripts/services.js
@@ -1,5 +1,7 @@
 //use jQuery
-
+let petSaloon={
+    service:[ ],
+}
 //create the constructor
 function Service(description, price){
     this.descritpion=description;
@@ -19,14 +21,36 @@ function isValid(service){
 }
 
 function register(){
-    console.log("Adding a new service");
+    console.log("Adding a new service");//displays in the console
     let inputDescription = $("#txtDescription").val();
     let inputPrice = $("#txtPrice").val();
-    let newService = new Service(inputDescription, inputPrice);
+    let newService = new Service(inputDescription, inputPrice);//create a new service
 
     if(isValid(newService)){
-        saveItem(newService);
+        saveItem(newService);//add new service
         $("input").val("");//clears the input fields
     
     }
+    displayServices();
+    
+}
+function displayServices(){
+
+    const petService=document.getElementById("petService");//gets the table
+        petService.innerHTML ="";
+for(let i=0;i<petSaloon.service.length;i++){//how many services are in the array
+    let service=petSaloon.service[i];//get the current service
+    
+let row =`
+    <tr>
+        <td>${pet.description}</td>//displays the description
+        <td>${pet.price}</td>//displays the price
+        <td><button class="btn btn-danger" onclick="deletePet(${i})">Delete</button></td>//
+        
+    </tr>
+    `;
+    
+    petService.innerHTML +=row;//insert the template into the HTML
+    }
+    
 }
